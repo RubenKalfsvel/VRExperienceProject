c35dc5ab563e1511141c5d694f43e07d39ce1e54
s151582 <s151582@ap.be>
2025-06-09 17:47:33 +0200

    Fix Screens and implement score functionality

--- Assets/ScoreManager/ScoreManager.cs
+++ Assets/ScoreManager/ScoreManager.cs
@@ -6,10 +6,16 @@ public class ScoreManager : MonoBehaviour
     [SerializeField] private int score = 0;
     [SerializeField] private int pointsPerKill = 10;
 
+    public GameManager gameManager;
+
     public void IncreaseScore(int points = -1)
     {
         int pointsToAdd = points == -1 ? pointsPerKill : points;
         score += pointsToAdd;
+        if(score >= 100)
+        {
+            gameManager.ShowVictoryScreen();
+        }
         Debug.Log($"Score increased by {pointsToAdd}! Current score: {score}");
     }
 
